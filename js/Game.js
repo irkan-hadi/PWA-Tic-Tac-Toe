702cb1472aa1539a71337bed4fea3bcd991e172a
irkan-hadi <ussr.specter@gmail.com>
2019-07-05 16:22:37 +0400

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -8,6 +8,11 @@ class Game {
             this.currentTurn = Game.X;
             this.MovesMade = 0;
             this.squares = new Array(9).fill().map(s => new Square());
+
+            // This is gonna be used to 
+            // determine who the winner is, 
+            // using Magic Square design instaed 
+            // of a predefined winning combinations
             this.squares[0].magicWeight = 2;
             this.squares[1].magicWeight = 7;
             this.squares[2].magicWeight = 6;
@@ -37,57 +42,57 @@ class Game {
             var usedQ = [];
             var matching_is;
 
-
+            // Put all the matching values in an array
             for (const sqr of this.squares) {
                 if (sqr.value === this.currentTurn) {
                     usedQ.push(sqr.magicWeight);
 
                 }
             }
-            //usedQ= usedQ.sort(function(a, b){return a - b});
+            
             var count = [];
+
 
             for (const wght_1 of usedQ) {
                 count.splice(0, count.length);
-
+                // Add the weight of the first element
                 magicSum = wght_1
-                
+                // Push it an array to keep track of the winning squares (to highlight them later)
                 count.push(wght_1);
                 masterLoop:
+                // Loop through the tagged squares to check their sums
                 for (const wght_2 of usedQ) {
+                    // Ignore if its the same square
                     if (wght_1 != wght_2) {
-
+                        // Sum it 
                         magicSum += wght_2;
+                        // Push it an array to keep track of the winning squares
                         count.push(wght_2);
 
-                        
+                        // If we have 3 elements that add up to 15 then we have a winner!
                         if (magicSum === 15 && count.length===3) {
 
                             this.inProgress = false;
                             this.winner = this.currentTurn;
+                            // Highlight the winning squares
                             for (const _sqr of this.squares) {
 
                                 _sqr.isHighlighted = count.includes(_sqr.magicWeight);
 
                             }
                             break masterLoop;
-                            //sqA.isHighlighted = sqb.isHighlighted = sqc.isHighlighted = true;
-
+                           
 
                         }
-                        else if (magicSum > 15 || count > 3) {
+                        // Break if sum reached > 15 or squares are more than 3
+                        else if (magicSum > 15 || count.length > 3) {
                             break masterLoop;
 
                         }
                     }
                 }
-
-
-
             }
-
- 
-            
+           
             if (this.MovesMade === this.squares.length) {
                 this.inProgress = false;
             }
@@ -96,10 +101,6 @@ class Game {
 
     }
 
-
-
-
-    
 }
 
 Game.O = 'O';
